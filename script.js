8df46cb732e64b4e8b4748ee08e2ab9df8051856
murpheycm <124391666+murpheycm@users.noreply.github.com>
2023-09-12 15:02:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const currentDate = new Date();
 let currentMonth = currentDate.getMonth();
 let currentYear = currentDate.getFullYear();
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
 
 // Function to create the calendar
 function generateCalendar(month, year) {
-  
+  const monthYear = document.getElementById('monthYear');
+  const dates = document.getElementById('dates');
+  const firstDay = new Date(year, month, 1);
+  const lastDay = new Date(year, month +1, 0);
 
-
+  dates.innerHTML = '';
+  monthYear.textContent = new Date(year, month).toLocaleString('default', 
+    {
+      month: 'long'
+    }
+  ) + ' ' + year;
 }
 
 generateCalendar(currentMonth,currentYear);
